c4b3718ab54ce7d56f1445ea2386825d0a16b036
zarrarrajput1010-byte <zarrarrajput1010@gmail.com>
2025-09-04 19:50:18 +0500

    Add files via upload

---
+++ Simple calculator.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	double num1, num2;
+	char op;
+	
+	cout << "Enter first number: ";
+	cin >> num1;
+	
+	cout << "Enter an operator (+, -, *, /): ";
+	cin >> op;
+	
+	cout << "Enter second number: ";
+	cin >> num2;
+	
+	if (op == '+') {
+		cout << "Result: " << num1 + num2 << endl;
+	}
+	else if (op == '-') {
+		cout << "Result: " << num1 - num2 << endl;
+	}
+	else if (op == '*') {
+		cout << "Result: " << num1 * num2 << endl;
+	}
+	else if (op == '/') {
+		if (num2 != 0) {
+			cout << "Result: " << num1 / num2 << endl;
+		}
+		else {
+			cout << "Error: Division by zero is not allowed." << endl;
+		}
+	}
+	else {
+		cout << "Error: Invalid operator." << endl;
+	}
+	
+	return 0;
+}
